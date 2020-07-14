cbe46df7595bc08ea5d023d0c3eacde3494d2981
juanmacivico87 <juanmacivico87@gmail.com>
2020-07-14 09:41:05 +0200

    Create route to edit the categories and link from list

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -27,7 +27,7 @@ class CategoryController extends AbstractController
     /**
      * @Route("/categories/new", name="app_category_new")
      */
-    public function createNewCategory(CategoryRepository $categoryRepository, EntityManagerInterface $entityManagerInterface, Request $request)
+    public function createNewCategory(EntityManagerInterface $entityManager, Request $request)
     {
         $category = new Category();
 
@@ -57,8 +57,47 @@ class CategoryController extends AbstractController
         $category->setName($categoryName);
         $category->setColour($categoryColour);
         // Save new category
-        $entityManagerInterface->persist($category);
-        $entityManagerInterface->flush();
+        $entityManager->persist($category);
+        $entityManager->flush();
+        // Display a success message
+        $this->addFlash( 'success', 'The new category has been saved' );
+        // Redirect to list of categories
+        return $this->redirectToRoute('app_categories_get');
+    }
+
+    /**
+     * @Route("/categories/edit/{id}", name="app_category_edit")
+     */
+    public function editCategory(Category $category, EntityManagerInterface $entityManager, Request $request)
+    {
+        if (!$this->isCsrfTokenValid('category', $request->request->get('categoryToken'))) {
+            return $this->render('category/edit.html.twig', [
+                'category' => $category,
+            ]);
+        }
+
+        if (!$categoryName = $request->request->get('categoryName', null)) {
+            // Display a error message
+            $this->addFlash( 'danger', 'Please, enter a name to the new category' );
+            return $this->render('category/edit.html.twig', [
+                'category' => $category,
+            ]);
+        }
+
+        if (!$categoryColour = $request->request->get('categoryColour', null)) {
+            // Display a error message
+            $this->addFlash( 'danger', 'Please, enter a colour to the new category' );
+            return $this->render('category/edit.html.twig', [
+                'category' => $category,
+            ]);
+        }
+
+        // Set fileds of the new category
+        $category->setName($categoryName);
+        $category->setColour($categoryColour);
+        // Save new category
+        $entityManager->persist($category);
+        $entityManager->flush();
         // Display a success message
         $this->addFlash( 'success', 'The new category has been saved' );
         // Redirect to list of categories
